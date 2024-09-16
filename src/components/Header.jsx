b044352be2840253ad08bc44a29546a92e1f041c
Agbooa12 <agboolaelijah077@gmail.com>
2024-09-16 21:27:48 +0100

    adding other section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,68 +3,77 @@ import logo from '../assets/logo.jpg'
 
 const Header = () => {
     return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">
-                    <img src={logo} alt='logo' />
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Our Service
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Same Day Delivery</a>
-                                <a class="dropdown-item" href="#">Courier Services</a>
-                                <a class="dropdown-item" href="#">Trucking</a>
-                                <a class="dropdown-item" href="#">Airfrieght</a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Solutions by industry
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Medical</a>
-                                <a class="dropdown-item" href="#">Automotive</a>
-                                <a class="dropdown-item" href="#">Aviation</a>
-                                <a class="dropdown-item" href="#">Retail & E-Commerce</a>
-                                <a class="dropdown-item" href="#">Industry</a>
-                                <a class="dropdown-item" href="#">Legal</a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Career
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Join Our Team</a>
-                                <a class="dropdown-item" href="#">Become Our Delivery Partner</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Us</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Resources
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Blog</a>
-                                <a class="dropdown-item" href="#">Events</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <>
+            <div className='bg-black'>
+                <nav className=" navbar navbar-expand-lg navbar-light text-white">
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} alt='logo' className='w-25' style={{ borderRadius: '100%' }} />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Our Service
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a className="dropdown-item" href="#">Same Day Delivery</a>
+                                    <a className="dropdown-item" href="#">Courier Services</a>
+                                    <a className="dropdown-item" href="#">Trucking</a>
+                                    <a className="dropdown-item" href="#">Airfrieght</a>
+                                </div>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Solutions by industry
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a className="dropdown-item" href="#">Medical</a>
+                                    <a className="dropdown-item" href="#">Automotive</a>
+                                    <a className="dropdown-item" href="#">Aviation</a>
+                                    <a className="dropdown-item" href="#">Retail & E-Commerce</a>
+                                    <a className="dropdown-item" href="#">Industry</a>
+                                    <a className="dropdown-item" href="#">Legal</a>
+                                </div>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Career
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a className="dropdown-item" href="#">Join Our Team</a>
+                                    <a className="dropdown-item" href="#">Become Our Delivery Partner</a>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-white" href="#">Contact Us</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-white" href="#">About Us</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Resources
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a className="dropdown-item" href="#">Blog</a>
+                                    <a className="dropdown-item" href="#">Events</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item ">
+                                <a className='border border-gray mr-5 rounded text-white p-2'>Request Quote</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+           
+        </>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,46 @@ import React from 'react'
 import Header from './Header'
 
 const Home = () => {
-  return (
-    <div>
-        <Header/>
-    </div>
-  )
+    return (
+        <>
+            <Header />
+
+            <div id='image'>
+                <h1>We prioritize humans first.</h1>
+                <p>Harness the power of advanced technology and world- class logistics solutions to drive big results for your business
+                    <button className='btn btn-white p-2'>Request Quote</button>
+                </p>
+            </div>
+
+            <div className='bg-lightgray'>
+                <div>
+                    <p>Empeiric is an American logistics and courier service company that is governed by expertise and technology. Our effective and efficient solutions help clients to reach point B from point A and get their loads delivered on time and within budget.</p>
+                </div>
+
+                <div>
+                    <p>You Allocate, We Deliver. We Provide Four Steps To Success</p>
+                    <div className='row'>
+                        <div className='col-lg-3'>
+                            <h2>Request and Schedule </h2>
+                            <p>Receive client requests via online portal or phone, collect details, and schedule pickups using route optimization software.</p>
+                        </div>
+                        <div className='col-lg-3'>
+                            <h2>Pickup </h2>
+                            <p>Verify client and item details, inspect items, secure items in the vehicle, and complete necessary documentation.</p>
+                        </div>
+                        <div className='col-lg-3'>
+                            <h2>Transport </h2>
+                            <p>Use GPS for real-time tracking and ensure compliance with safety protocols.</p>
+                        </div>
+                        <div className='col-lg-3'>
+                            <h2>Delivery </h2>
+                            <p>Verify recipient, inspect items, complete documentation, and secure handoff. Update system and notify the client.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home
