208b7400ca4b94b13992d5a5816547c90da2332e
Agbooa12 <agboolaelijah077@gmail.com>
2024-09-18 14:36:56 +0100

    wip on the design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Header from './Header'
+import one from '../assets/02.jpg'
+import two from '../assets/01.jpg'
+import zero from '../assets/00.jpg'
 
 const Home = () => {
     return (
@@ -48,6 +51,62 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+
+            <div className='container mt-5'>
+                <p className='fs-2 text-center fw-bold'>We provide Best on Demand Logistics and Courier Services </p>
+
+                <div className='row'>
+                    <div className='col-lg-6'>
+                        <div className='card shadow-sm '>
+                            <img src={one} alt="two" className='w-100' />
+                            <div className='card-body'>
+                                <p className='fw-bold fs-3'>Companies</p>
+                                <p>
+                                    Whether you’re a small business or a large enterprise, we have the software and solutions that you can tailor to your business to easily drive improvements.
+                                </p>
+                                <a href='#' className='text-black fs-5'>Request Quote</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-lg-6'>
+                        <div className='card shadow-sm '>
+                            <img src={two} alt="two" className='w-100' />
+                            <div className='card-body'>
+                                <p className='fw-bold fs-3'>Shippers</p>
+                                <p>
+                                    Empeiric is an American logistics and courier service company that is governed by expertise and technology. Our effective and efficient solutions help clients to reach point A from point B and get their loads delivered on time and within budget.
+                                </p>
+                                <a href='#' className='text-black fs-5'>Request Quote</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='images mt-5'>
+                <div className='move'>
+                    <p className='fw-bold fs-1 text-white'>Move a load today </p>
+                    <button className='btn btn-black bg-dark rounded text-white p-2'>Get Instant Quote</button>
+                </div>
+            </div>
+
+            <div className='container m-5'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <img src={zero} alt='show' className='w-100' />
+                    </div>
+                    <div className='col-md-6 mt-5'>
+                        <p className='fs-3 fw-bold text-center'> Optimize every aspect of your supply chain </p>
+                        <p className=''>
+                            Our comprehensive range of managed services empower you to harness the full potential of advanced technology and the world’s best logistics experts to optimize your supply chain operations. Outsource all your transportation needs or choose from our menu of add-on logistics solutions and services.
+                        </p>
+                    <button className='btn btn-black bg-dark rounded-sm text-white p-2'>Request A Quote</button>
+                    </div>
+                </div>
+
+            </div>
+
         </>
     )
 }
