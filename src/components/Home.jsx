429f796af1615e3c0dac5afeeba38f7bcd984b24
Agbooa12 <agboolaelijah077@gmail.com>
2024-09-18 13:13:34 +0100

    wip on the design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,40 +8,45 @@ const Home = () => {
 
             <div id='image'>
                 <div className='head'>
-                 <p className='fs-1 fw-bold'>We prioritize humans first.</p>
-                <p className='fs-5'>Harness the power of advanced technology and world- class logistics solutions to drive big results for your business
-                </p>
-                    <button className='btn btn-white bg-white fw-bold fs-5 p-2 text-black'>Request Quote</button> 
+                    <p className='fs-1 fw-bold'>We prioritize humans first.</p>
+                    <p className='fs-5'>Harness the power of advanced technology and world- class logistics solutions to drive big results for your business
+                    </p>
+                    <button className='btn btn-white bg-white fw-bold fs-5 p-2 text-black'>Request Quote</button>
                 </div>
             </div>
 
-            <div className='bg-lightgray'>
-                <div className='container fs-5 mt-5 fw-bold'>
-                    <p>Empeiric is an American logistics and courier service company that is governed by expertise and technology. Our effective and efficient solutions help clients to reach point B from point A and get their loads delivered on time and within budget.</p>
-                </div>
 
-                 <div>
-                    <p>You Allocate, We Deliver. We Provide Four Steps To Success</p>
-                    <div className='row'>
+            <div className='container fs-5 mb-5 mt-5 fw-bold'>
+                <p>Empeiric is an American logistics and courier service company that is governed by expertise and technology. Our effective and efficient solutions help clients to reach point B from point A and get their loads delivered on time and within budget.</p>
+            </div>
+
+            <div className=' jumbotron'>
+                <div className='container'>
+
+                    <p className='text-center fs-3'>You Allocate, We Deliver. We Provide Four Steps To Success</p>
+                    <div className='row mt-5'>
                         <div className='col-lg-3'>
-                        <i className="fa-solid fa-truck"></i>
+                            <i className="fs-3 fa-solid fa-truck"></i>
                             <h2>Request and Schedule </h2>
                             <p>Receive client requests via online portal or phone, collect details, and schedule pickups using route optimization software.</p>
                         </div>
                         <div className='col-lg-3'>
+                            <i className="fs-3 fa-solid fa-truck"></i>
                             <h2>Pickup </h2>
                             <p>Verify client and item details, inspect items, secure items in the vehicle, and complete necessary documentation.</p>
                         </div>
                         <div className='col-lg-3'>
+                            <i className="fs-3 fa-solid fa-truck"></i>
                             <h2>Transport </h2>
                             <p>Use GPS for real-time tracking and ensure compliance with safety protocols.</p>
                         </div>
                         <div className='col-lg-3'>
+                            <i className="fs-3 fa-solid fa-truck"></i>
                             <h2>Delivery </h2>
                             <p>Verify recipient, inspect items, complete documentation, and secure handoff. Update system and notify the client.</p>
                         </div>
                     </div>
-                </div> 
+                </div>
             </div>
         </>
     )
